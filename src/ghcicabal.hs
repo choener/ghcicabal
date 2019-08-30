0e9657154b7ee84614023ad7614ae5dc642f307e
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-08-31 00:08:52 +0200

    more cleanup

--- src/ghcicabal.hs
+++ src/ghcicabal.hs
@@ -1,26 +1,33 @@
 
+-- | Calls @ghci@ with all extensions of all subprojects, and also provides all
+-- such paths to @ghci. This makes it possible to develop multiple packages,
+-- that are interdependent, in parallel.
+
 module Main where
 
+import Data.List (intersperse)
 import Distribution.PackageDescription.Parsec
-import Distribution.Verbosity
-import Distribution.Types.GenericPackageDescription
-import Distribution.Types.Library
+import Distribution.Pretty
 import Distribution.Types.BuildInfo
 import Distribution.Types.CondTree
-import Distribution.Types.ForeignLib
 import Distribution.Types.Executable
+import Distribution.Types.ForeignLib
+import Distribution.Types.GenericPackageDescription
+import Distribution.Types.Library
+import Distribution.Verbosity
+import GHC.Generics(Generic)
 import Language.Haskell.Extension
 import qualified Data.Set as S
 import qualified System.Process as P
-import Text.Printf
-import Distribution.Pretty
-import Data.List (intersperse)
 import System.Environment
-import System.FilePath.Find as F
-import GHC.Generics(Generic)
 import System.FilePath
+import System.FilePath.Find as F
+import Text.Printf
 
 
+
+-- | Extract the necessary information from all packages. Two @Info@s can be
+-- combined using @<>@, and the final @Info@ yields the correct environment.
 
 data Info = Info
   { iExts ∷ S.Set Extension
@@ -34,16 +41,16 @@ instance Semigroup Info where
 instance Monoid Info where
   mempty = Info S.empty S.empty
 
+-- | Creates an 'Info' from a 'BuildInfo'.
+
 mkInfo ∷ FilePath → BuildInfo → Info
 mkInfo f BuildInfo{..} = Info (S.fromList $ defaultExtensions ++ otherExtensions) (S.fromList $ map (dropFileName f </>) hsSourceDirs)
 
 
--- | returns all extensions used in the cabal file
+-- | Given a filepath and a package description, return the 'Info'.
 
 extensions ∷ FilePath → GenericPackageDescription → Info
 extensions f p = cl <> mconcat csls <> mconcat cfls <> mconcat ces
---    ++ cfls
---    ++ ces
     -- TODO test suites and benchmarks
   where
     lib = libBuildInfo . condTreeData
@@ -52,27 +59,23 @@ extensions f p = cl <> mconcat csls <> mconcat cfls <> mconcat ces
     cfls = [ mkInfo f . foreignLibBuildInfo $ condTreeData t | (_,t) ← condForeignLibs p ]
     ces  = [ mkInfo f . buildInfo $ condTreeData t | (_,t) ← condExecutables p ]
 
+-- | Prepare 'Extension' as @ghci@ argument.
+
 extString ∷ Extension → String
 extString = ("-X" ++) . prettyShow
 
-withDir ∷ String → String
-withDir = ("-i" ++)
-
 main = do
-  ghcLibDir ← maybe "" id <$> lookupEnv "NIX_GHC_LIBDIR"
   as ← getArgs
   let ds = if null as then ["./"] else as
   fs ← concat <$> mapM (F.find always (extension ==? ".cabal")) ds
-  mapM_ print fs
   ps ← mapM (readGenericPackageDescription silent) fs
   let z = mconcat $ zipWith extensions fs ps
-  print z
   -- cobble together the command line
-  let cmdline = printf "ghci %s %s"
+  let cmdline = printf "ghci %s -i%s"
         -- all extensions
         (concat . intersperse " " . map extString . S.toList $ iExts  z)
         -- all directories, extracted from cabal files
-        (concat . intersperse " " . map withDir   . S.toList $ iPaths z)
+        (concat . intersperse ":" . S.toList $ iPaths z)
   putStrLn cmdline
   P.callCommand cmdline
 
