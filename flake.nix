2e59f3766fb7d61d0d3b9b0794edaabbed5b2591
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2021-01-24 14:43:00 +0100

    flake for our little build tool

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "flake that provides ghcicabal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (
+    system: let
+      pkgs = import nixpkgs { inherit system; overlays = [ ghcicabal ]; };
+      ghcicabal = self: super: {
+        ghcicabal = self.haskellPackages.callPackage ./default.nix {};
+      };
+    in {
+      defaultPackage = pkgs.ghcicabal;
+    }
+  );
+
+}
