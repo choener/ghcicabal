2ee6ec98c56098a9c6c07c0964c2cdfb88a47446
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2023-04-20 11:08:15 +0200

    updated flake for better dev experience

--- flake.nix
+++ flake.nix
@@ -8,13 +8,24 @@
 
   outputs = { self, nixpkgs, flake-utils }: let
     over = final: prev: {
-      ghcicabal = final.haskellPackages.callPackage ./default.nix {};
+      haskellPackages = (prev.haskellPackages.override { overrides = hself: hsuper: {
+        ghcicabal = final.haskellPackages.callPackage ./default.nix {};
+      }; });
     };
   in flake-utils.lib.eachDefaultSystem (
     system: let
       pkgs = import nixpkgs { inherit system; overlays = [ over ]; };
     in {
-      defaultPackage = pkgs.ghcicabal;
+      devShell = pkgs.haskellPackages.shellFor {
+        packages = p: [ p.ghcicabal ];
+        withHoogle = true;
+        buildInputs = with pkgs; with pkgs.haskellPackages; [ cabal-install implicit-hie haskell-language-server ];
+      }; # devShell
+
+      apps = {
+        ghcicabal = { type = "app"; program = "${pkgs.haskellPackages.ghcicabal}/bin/ghcicabal"; };
+        buildcabal = { type = "app"; program = "${pkgs.haskellPackages.ghcicabal}/bin/buildcabal"; };
+      };
     }
   ) // { overlay = over; };
 
